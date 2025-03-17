d62bffa0c29510a8a72d180d2a74b8a33104e7f2
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-03-17 13:56:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// String concatenation is a way to take two strings and add one to the other, creating a single, longer string using the + operator
+
+// String interpolation lets us dynamically insert values in the middle of a string. To do this, we need to use  strings wrapped in backticks rather than single or double quotes. e.g.  '$ {myString}
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ' +  ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ' +  ${currentUser.slice(0,1)}!`;
